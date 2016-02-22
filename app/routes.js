4d69248854eb7cb65f7521c70b5949cffc12fa01
Navaneet <navaneetsingh3112@gmail.com>
2016-02-22 18:06:16 +0530

    Added mongodb configuration

---
+++ app/routes.js
@@ -0,0 +1,13 @@
+/*app.post('/cms/auth/login',function(req,res){
+	res.send('Got a POST request');
+});
+*/
+//var express        = require('app');
+
+// routes
+	//app.use('/cms/auth', require('./controllers/AppLoginController'));
+
+
+/*module.exports = function(app) {
+	routes.initialize(app);
+}*/
--- server.js
+++ server.js
@@ -3,16 +3,53 @@
 var express        = require('express');
 var app            = express();
 var path           = require('path');
+var bodyParser     = require('body-parser');
+var mongoose       = require('mongoose');
 
+// index page loading ======================================
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname + '/public/LoginIndex.html'));
 });
 
-app.post('/cms/auth/login',function(req,res){
-	res.send('Got a POST request');
+
+// configuration ===========================================
+var db = require('./config/db');
+var port = 8080; 
+// connect to mongoDB database
+mongoose.connect(db.url);
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+    
+// routes ==================================================
+var Schema = mongoose.Schema;
+var db = mongoose.connection;
+
+var users = new Schema({
+  name:  String,
+  password: String
 });
 
-app.listen(8080,function(){
+var Users = mongoose.model('users', users);
+
+
+app.post('/cms/auth/login', function(req,res){
+
+	Users.find({name: '1111' , password: '1111'}, function (err, docs) {
+        console.log("login -----"+JSON.stringify(docs));
+    });
+	
+});
+
+
+app.listen(port,function(){
 
 });
+
+
